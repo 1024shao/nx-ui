ca264025496746454b26a449465e87b131d3163f
VuJson <3059680677@qq.com>
2022-03-08 00:01:45 +0800

    version 1.0

---
+++ packages/index.js
@@ -0,0 +1,35 @@
+import NxButton from './nx-button/index.vue'
+import NxDialog from './nx-dialog/index.vue'
+import NxInput from './nx-input/index.vue'
+import NxSwitch from './nx-switch/index.vue'
+import NxRadio from './nx-radio/index.vue'
+import NxRadioGroup from './nx-radio-group/index.vue'
+import NxCheckBox from './nx-checkbox/index.vue'
+import NxCheckBoxGroup from './nx-checkbox-group/index.vue'
+import NxForm from './nx-form/index.vue'
+import NxFormItem from './nx-form-item/index.vue'
+import './fonts/iconfont.css'
+const components = [
+  NxButton,
+  NxDialog,
+  NxInput,
+  NxSwitch,
+  NxRadio,
+  NxRadioGroup,
+  NxCheckBox,
+  NxCheckBoxGroup,
+  NxForm,
+  NxFormItem,
+]
+const install = (Vue) => {
+  components.forEach(item => {
+    Vue.component(item.name, item)
+  })
+}
+// script 标签引入
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+export default {
+  install
+}
--- src/main.js
+++ src/main.js
@@ -1,26 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './packages/fonts/iconfont.css'
-import NxButton from './packages/nx-button/index.vue'
-import NxDialog from './packages/nx-dialog/index.vue'
-import NxInput from './packages/nx-input/index.vue'
-import NxSwitch from './packages/nx-switch/index.vue'
-import NxRadio from './packages/nx-radio/index.vue'
-import NxRadioGroup from './packages/nx-radio-group/index.vue'
-import NxCheckBox from './packages/nx-checkbox/index.vue'
-import NxCheckBoxGroup from './packages/nx-checkbox-group/index.vue'
-import NxForm from './packages/nx-form/index.vue'
-import NxFormItem from './packages/nx-form-item/index.vue'
-
+import NxUI from '../dist/next-ui.es.js'
+import '../dist/style.css'
 const app = createApp(App)
-app.component(NxButton.name, NxButton)
-app.component(NxDialog.name, NxDialog)
-app.component(NxInput.name, NxInput)
-app.component(NxSwitch.name, NxSwitch)
-app.component(NxRadio.name, NxRadio)
-app.component(NxRadioGroup.name, NxRadioGroup)
-app.component(NxCheckBox.name, NxCheckBox)
-app.component(NxCheckBoxGroup.name, NxCheckBoxGroup)
-app.component(NxForm.name, NxForm)
-app.component(NxFormItem.name, NxFormItem)
+app.use(NxUI)
 app.mount('#app')
